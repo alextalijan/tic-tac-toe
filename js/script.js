4e115bdf2125abb1b70f4baff6cc3e349c40ffe2
alextalijan <acatalijan@icloud.com>
2025-05-28 15:11:19 -0500

    refactor: remove newGame method, add it to playGame
    There was no reason for new game to begin, so it has been included at
    the start of the playGame method. Some other changes have been made
    regarding visually interacting with the DOM, but it hasn't been fully
    implemented.

--- js/script.js
+++ js/script.js
@@ -45,14 +45,13 @@ const Gameboard = (function () {
         // Check if the move is legal
         if (move.length === 2 && move[0] >= 0 && move[0] <= 2 && move[1] >= 0 && move[1] <= 2) {
             // If the move is available, make it, else tell them to pick another
-            if (getAvailableMoves().some(availableMove => availableMove[0] === playerMove[0] && availableMove[1] === playerMove[1])) {
-                console.log(`Player ${turnPlayer()} has made a move.`);
+            if (getAvailableMoves().some(availableMove => availableMove[0] === move[0] && availableMove[1] === move[1])) {
                 board[move[0]][move[1]] = turnPlayer();
             } else {
-                console.log("That move has already been played. Pick another one.");
+                alert("That move has already been played. Pick another one.");
             }
         } else {
-            console.log("Out of bounds move. Please provide a legal one.");
+            alert("Out of bounds move. Please provide a legal one.");
         }
     };
 
@@ -68,22 +67,14 @@ const Gameboard = (function () {
 })();
 
 const Game = (function () {
-    
-    const newGame = function () {
-        Gameboard.clearBoard();
-        console.log("Starting new game...");
-        playGame();
-    };
 
     const isGameOver = function () {
         // If there is a winner, return it
         // Check every row and column for 3 in a row
         for (let i = 0; i < 3; i++) {
             if (Gameboard.board[i][0] === Gameboard.board[i][1] && Gameboard.board[i][1] === Gameboard.board[i][2] && Gameboard.board[i][0] !== "EMPTY") {
-                console.log(`The winner is ${Gameboard.board[i][0]}.`);
                 return Gameboard.board[i][0];
             } else if (Gameboard.board[0][i] === Gameboard.board[1][i] && Gameboard.board[1][i] === Gameboard.board[2][i]  && Gameboard.board[0][i] !== "EMPTY") {
-                console.log(`The winner is ${Gameboard.board[0][i]}.`);
                 return Gameboard.board[0][i];
             }
         }
@@ -105,19 +96,7 @@ const Game = (function () {
         }
     }
 
-    const playRound = function () {
-        console.log(`${Gameboard.turnPlayer()}, it's your turn.`);
-
-        // Prompt the user continuosly for the move in a valid format
-        while (true) {
-            playerMove = prompt("Please input your move in following format: 1,2").split(",").map(stringNumber => parseInt(stringNumber));
-            if (playerMove.length === 2) {
-                break;
-            }
-
-            console.log("This move doesn't exist. Make sure to respect the format - '0,0', '1,0'...");
-        }
-
+    const playRound = function (playerMove) {
         // Show the board result of the round
         console.clear();
         Gameboard.makeMove(playerMove);
@@ -125,8 +104,14 @@ const Game = (function () {
     };
 
     const playGame = function () {
+        // Initiate the start of a new game
+        Gameboard.clearBoard();
+        displayController.render();
+        alert("Starting new game...");
+
         // While the game isn't done, play a round
         while (!isGameOver()) {
+            alert(`${Gameboard.turnPlayer()}, it's your turn.`);
             playRound();
         }
 
@@ -135,29 +120,30 @@ const Game = (function () {
         // If there is a winner, announce him, else it's a tie
         // The isGameOver() function returns a winner if existing
         if (playerIcons.includes(isGameOver())) {
-            console.log(`The winner is ${isGameOver()}!`);
+            alert(`The winner is ${isGameOver()}!`);
         } else {
-            console.log("It's a tie.");
+            alert("It's a tie.");
         }
     };
 
-    return {newGame};
+    return {playGame, playRound};
 })();
 
-const displayController = (function () {
-    const gameDisplay = document.querySelector(".game-display");
+const gameDisplay = document.querySelector(".game-display");
 
+const displayController = (function () {
     const render = function () {
         // Clear the current display
         gameDisplay.innerHTML = "";
 
         // Print every field
-        for (const row of Gameboard.board) {
-            for (const field of row) {
+        for (let i = 0; i < Gameboard.board.length; i++) {
+            for (let j = 0; j < Gameboard.board[i].length; j++) {
                 const newFieldDiv = document.createElement("div");
+                newFieldDiv.dataAttribute = `${i},${j}`;
 
-                if (field !== "EMPTY") {
-                    newFieldDiv.textContent = field;
+                if (Gameboard.board[i][j] !== "EMPTY") {
+                    newFieldDiv.textContent = Gameboard.board[i][j];
                 }
 
                 gameDisplay.appendChild(newFieldDiv);
@@ -165,5 +151,18 @@ const displayController = (function () {
         }
     };
 
+    gameDisplay.addEventListener("click", (event) => {
+        // Set the proper icon of the player inside the clicked field
+        Game.playRound(event.target.dataAttribute.split(",").map(stringNumber => parseInt(stringNumber)));
+        render();
+    });
+
     return {render};
 })();
+
+// When the new game button is clicked, start the new game
+const newGameButton = document.querySelector(".new-game-btn");
+newGameButton.addEventListener("click", () => {
+    gameDisplay.style.display = "grid";
+    Game.newGame();
+});
